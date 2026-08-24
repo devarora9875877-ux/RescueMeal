7e18bf5591bf328e05877ae5a3bda95530b00150
Minalpreet Singh <minalpreet3279.beai25@chitkara.edu.in>
2026-08-24 12:14:31 +0530

    Add files via upload

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,5 +1,19 @@
+import React from "react";
+import "./NotFound.css";
+import not from "../assets/images/not-found.png"
+
 function NotFound() {
-  return <h1>404 Page Not Found</h1>;
+  return (
+    <div className="notfound-full">
+      <img 
+        src={not} 
+        alt="404 Error Page Not Found"
+        className="notfound-full-image"
+      />
+    </div>
+  );
 }
 
 export default NotFound;
+
+
